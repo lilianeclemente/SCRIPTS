83cc8f48a2bad0bd8b3b169ae32b060a21950d99
F4NT0 <gabriel.stundner@acad.pucrs.br>
2019-09-20 02:01:28 -0300

    Shortcuts para Desktop

---
+++ Shortcuts/Desktop_Shortcut.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#--------------------------------
+# CRIANDO UM SHORTCUT NO DESKTOP
+#--------------------------------
+
+# BAIXANDO O PROGRAMA PARA CRIAR O PROGRAMA
+
+sudo apt  install --no-install-recommends gnome-panel
+
+
+# CRIANDO UM SHORTCUT NO DESKTOP
+
+gnome-desktop-item-edit ~/Desktop/ --create-new
+
+
